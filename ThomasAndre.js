58ab0cea6e2f2719bc3a0628bd0b7307975d3b5c
GET Academy <terje@kolderup.net>
2024-04-23 13:08:29 +0200

    Initial commit

---
+++ ThomasAndre.js
@@ -0,0 +1,15 @@
+var ProfileAccessPermRoles = {
+    "Get": {
+        "Start It": {
+            "Team 1": {
+                "subRoles": ["StartItLærer", "Veiledere", "Kun meg selv"]
+            },
+            "Team 2": {
+                "subRoles": ["StartItLærer", "Veiledere", "Kun meg selv"]
+            },
+            "Team 3": {
+                "subRoles": ["StartItLærer", "Veiledere", "Kun meg selv"]
+            },
+        }
+    }
+};
